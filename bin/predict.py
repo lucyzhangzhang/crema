cfbfa39c8551b244a792d1075a206c78b7f206fe
lucyzhangzhang <lzsj1995@gmail.com>
2019-11-25 21:09:38 -0500

    option to specify output directory

--- bin/predict.py
+++ bin/predict.py
@@ -16,6 +16,13 @@ import os
 # Import fasta, cpat, diamond files from cmnd line #
 ####################################################
 
+# function to test if output directory string is a directory
+def dir_path(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError(string)
+
 usage = "usage: %prog [options] -f transcript.fa -c cpat.csv -d diamond.out"
 
 parser = argparse.ArgumentParser(description="predict lncRNAs from transcript sequences")
@@ -23,6 +30,9 @@ parser.add_argument("-f", "--fasta", dest="trans_fasta", required=True, metavar=
 parser.add_argument("-c", "--cpat_out", dest="cpat_out", required=True, metavar="cpat_out.csv", help="CPAT output of transcript sequences")
 parser.add_argument("-d", "--diamond_out", dest="diam_out", required=True, metavar="diamond_out.tab", help="Diamond output of transcript sequences")
 parser.add_argument("-s", "--score_cutoff", dest="score_cut", type=float, default=0.5, metavar="s", help="Minimum cutoff score for the lncRNA prediction (Default: 0.5)")
+parser.add_argument("-o", "--outDir", dest = "outDir",
+        required = False, metavar = "Output directory location",
+        type = dir_path, help = "Location to put the output files")
 
 args = parser.parse_args()
 
@@ -31,6 +41,13 @@ CURRENT_DIR = os.path.dirname(__file__)
 
 #path of the inputfiles
 fasta_path = os.path.dirname(args.trans_fasta)
+
+"""set path of outputs to the location of the input fasta
+if no output directory (-o, --outDir) is specified"""
+if args.outDir == 0:
+    out = fasta_path
+else:
+    out = args.outDir
 
 #os.path.join(CURRENT_DIR+'../gb_models
 clf1 = joblib.load(os.path.join(CURRENT_DIR+'/../updated_gb_models/model1.pkl'))
@@ -71,14 +88,14 @@ for x in model_names:
     probability[model_name] = lnc_score.tolist()
     prediction[model_name] = pred.tolist()
 
-prediction.to_csv(os.path.join(fasta_path,"all_model_predictions.csv"), sep=",")
-probability.to_csv(os.path.join(fasta_path,"all_model_scores.csv"), sep=",")
+prediction.to_csv(os.path.join(out,"all_model_predictions.csv"), sep=",")
+probability.to_csv(os.path.join(out,"all_model_scores.csv"), sep=",")
 
 
 ## Run logistic regression in R on model scores
 CURRENT_DIR = os.path.dirname(__file__)
 path_to_Rscript = os.path.join(CURRENT_DIR+'/log_reg.R')
-cmnd_to_run = 'Rscript ' + path_to_Rscript + ' -m ' + os.path.join(fasta_path, "all_model_predictions.csv") + ' -t GB'
+cmnd_to_run = 'Rscript ' + path_to_Rscript + ' -m ' + os.path.join(out, "all_model_predictions.csv") + ' -t GB'
 os.system(cmnd_to_run)
 
 # empty pandas df 
@@ -88,7 +105,7 @@ os.system(cmnd_to_run)
 # create pandas df with all features and log reg output
 # add to trans_dict?
 
-with open(os.path.join(fasta_path,"ensemble_logreg_pred.csv")) as pred:
+with open(os.path.join(out,"ensemble_logreg_pred.csv")) as pred:
     pred_reader = csv.reader(pred, delimiter = ",")
     next(pred_reader) 
     for row in pred_reader:
@@ -107,7 +124,7 @@ feature_df = feature_df[['length', 'ORF', 'GC', 'fickett', 'hexamer', 'identity'
 # count number of predicted lncRNAs:
 lnc_num = sum(x == 1 for x in feature_df['prediction'])
 
-feature_df.to_csv(os.path.join(fasta_path, "final_ensemble_predictions.csv"), sep=",")
+feature_df.to_csv(os.path.join(out, "final_ensemble_predictions.csv"), sep=",")
 
 print("Run successful!")
 print("There were " + str(lnc_num) + " lncRNAs predicted from " + args.trans_fasta)
